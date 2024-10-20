0191acb819f44387299a1c7b34974e41bc58e3e5
Francisco Santos <fwalbergsantos@outlook.com>
2024-10-20 20:18:01 +0000

    Cart.js controller finished

--- cartridges/app_custom_cart_page_message/cartridge/controllers/Cart.js
+++ cartridges/app_custom_cart_page_message/cartridge/controllers/Cart.js
@@ -1,18 +1,38 @@
 const server = require('server');
-const Cart = module.superModule;
-
-server.extend(Cart);
+server.extend(module.superModule);
 
 server.append('Show', (req, res, next) => {
+
     const BasketMgr = require('dw/order/BasketMgr');
-    const currentBasket = BasketMgr.getCurrentBasket();
+    const Site = require('dw/system/Site');
+
+    const currentBasket : Basket = BasketMgr.getCurrentBasket();
+
+    if (!currentBasket) {
+        return next();
+    }
+
+    if (currentBasket.productLineItems.length === 0) {
+        return next();
+    }
+
     const carTotal = currentBasket.totalGrossPrice.value;
+    const cartThreshold = Site.getCurrent().getCustomPreferenceValue('cartTotalThreshold');
+    const isThresholdExceeded = carTotal >= cartThreshold;
 
+    let viewData = res.getViewData();
+    if (isThresholdExceeded) {
+        const cartMessage = Site.getCurrent()
+            .getCustomPreferenceValue('cartTotalThresholdMessage')
+            .concat(cartThreshold);
 
-    res.setViewData({
-        customMessage: "Custom message test!!!",
-        carTotal: carTotal
-    });
+        viewData.cartMessage = cartMessage;
+        viewData.isThresholdExceeded = isThresholdExceeded;
+    }
+
+    res.setViewData(viewData);
 
     next();
 });
+
+module.exports = server.exports();
