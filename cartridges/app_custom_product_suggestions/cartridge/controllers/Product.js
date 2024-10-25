27f46ed57622e3e4c2b89a9dcf1b49004f9ee413
Francisco Santos <fwalbergsantos@outlook.com>
2024-10-25 17:18:26 +0000

    created helper file, adjustments on Controller, and scss file

--- cartridges/app_custom_product_suggestions/cartridge/controllers/Product.js
+++ cartridges/app_custom_product_suggestions/cartridge/controllers/Product.js
@@ -4,26 +4,12 @@ const server = require('server');
 server.extend(module.superModule);
 
 server.append('Show', (req, res, next) => {
-    const ProductSearchModel = require('dw/catalog/ProductSearchModel');
     const ProductMgr = require('dw/catalog/ProductMgr')
-    const CatalogMrg = require('dw/catalog/CatalogMgr')
+    const productHelper = require('*/cartridge/scripts/helpers/productSuggestion.js');
 
     const product = ProductMgr.getProduct(req.querystring.pid);
-    const productCategory = product.variant ? product.variationModel.defaultVariant
-        .masterProduct.primaryCategory.ID : product.primaryCategory.ID;
+    const suggestedProducts = product ? productHelper.getProductSuggestions(product) : [];
 
-    const productSearchModel = new ProductSearchModel();
-    productSearchModel.setCategoryID(productCategory);
-    productSearchModel.search();
-    
-    const maxProductHits = 4;
-    const suggestedProducts = productSearchModel
-        .getProductSearchHits()
-        .asList().toArray()
-        .filter(hit => hit.getProduct().ID != product.ID)
-        .slice(0, maxProductHits)
-        .map(hit => hit.getProduct());
-    
     res.setViewData({
         suggestedProducts: suggestedProducts,
     });
